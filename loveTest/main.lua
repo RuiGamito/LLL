08e002ddd31556d3f8886cf8c75b2c9f04054c8d
RicardoMBCGama <ricardombgama@gmail.com>
2017-03-17 23:43:45 +0000

    no message

--- loveTest/main.lua
+++ loveTest/main.lua
@@ -8,22 +8,34 @@ initialPosX = windowWidth/2
 initialPosY = windowHeigth/2
 circleSpeed = 300
 
+rectangleWidth = 100
+rectangleHeigth = 20
+rectangleSpeed = 500
+
 
 
 -- Game Cycle
 function love.load()
-  posX = initialPosX
-  posY = initialPosY
+  circlePosX = initialPosX
+  circlePosY = initialPosY
+  rectanglePosX = rectangleWidth/2
+  rectanglePosY = windowHeigth/2
 end
 
 function love.update(dt)
   handleInput(dt)
+  moveRectangle(dt)
 end
 
 
 function love.draw()
   love.graphics.print("Hello World", 400, 300)
-  love.graphics.circle("fill", posX, posY, circleRadius)
+
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.circle("fill", circlePosX, circlePosY, circleRadius)
+
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.rectangle("fill", rectanglePosX, rectanglePosY, rectangleWidth, rectangleHeigth)
 end
 
 
@@ -33,24 +45,32 @@ end
 -- Keyboard Handling
 function handleInput(dt)
   if love.keyboard.isDown("up") then
-    posY = posY - circleSpeed * dt
-    if posY - circleRadius < 0 then
-      posY = circleRadius
+    circlePosY = circlePosY - circleSpeed * dt
+    if circlePosY - circleRadius < 0 then
+      circlePosY = circleRadius
     end
   elseif love.keyboard.isDown("down") then
-    posY = posY + circleSpeed * dt
-    if posY + circleRadius > windowHeigth then
-      posY = windowHeigth - circleRadius
+    circlePosY = circlePosY + circleSpeed * dt
+    if circlePosY + circleRadius > windowHeigth then
+      circlePosY = windowHeigth - circleRadius
     end
   elseif love.keyboard.isDown("right") then
-    posX = posX + circleSpeed * dt
-    if posX + circleRadius > windowWidth then
-      posX = windowWidth - circleRadius
+    circlePosX = circlePosX + circleSpeed * dt
+    if circlePosX + circleRadius > windowWidth then
+      circlePosX = windowWidth - circleRadius
     end
   elseif love.keyboard.isDown("left") then
-    posX = posX - circleSpeed * dt
-    if posX - circleRadius < 0 then
-      posX = circleRadius
+    circlePosX = circlePosX - circleSpeed * dt
+    if circlePosX - circleRadius < 0 then
+      circlePosX = circleRadius
     end
+  end
+end
+
+function moveRectangle(dt)
+  if rectanglePosX > windowWidth + rectangleWidth/2 then
+    rectanglePosX = -rectangleWidth/2
+  else
+    rectanglePosX = rectanglePosX + rectangleSpeed * dt
   end
 end
