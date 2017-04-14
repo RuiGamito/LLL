1425ee91934d07ede2f438524fae4f2091aae312
Rui Gamito <atrium601@gmail.com>
2017-04-14 15:29:50 +0100

    Initial commit for TheCrusher
    - Blocks move from right to left
    - Blocks change color to reflect how close they are to crushing (from green to orange)
    - Blocks recede, after hitting the ground

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LLL
-Learning Love &amp; Lua 
+Learning Love &amp; Lua
+
+
+TheCrusher
+A game where must avoid getting hit by the never-ending crushing blocks.
--- main.lua
+++ main.lua
@@ -1,141 +1,93 @@
-require "hud"
 
-pos_x = 100
-pos_y = 200
+pos_x = 500
+pos_y = 500
 bsize_x = 50
 bsize_y = 50
 
-block_pos_x = 500
-block_pos_y = 500
+block_pos_x = 750
+block_pos_y = 0
 
-window_w = love.graphics.getWidth()
-window_h = love.graphics.getHeight()
+block_size_x = 100
+block_size_y = 50
 
-hud_height = 30
+local blocks = {}
+local num_blocks = 0
 
-shape_collider = love.physics.newRectangleShape(pos_x, pos_y, bsize_x, bsize_y)
+-- BLOCK = {XX, YY, WIDTH, HEIGHT, CRUSH_TRIGGER, STATUS}
+-- STATUS = 0 -> No change
+--          1 -> Crushing
+--          2 -> Receding
 
+function spawnBlock()
+  return {800, 0, block_size_x, block_size_y, love.math.random(0,1000)+500, 0}
+end
 
--- Game states
-
-STATE_MENU      = 1
-STATE_PLAY      = 2
-STATE_GAME_OVER = 3
-
--- Game Cycle Functions
 
 function love.load()
+  local b1 = spawnBlock()
+  table.insert(blocks, b1)
+  num_blocks = 1
+end
 
-  -- initialize HUD
-  hud:init()
-  hud:set_hud_height(hud_height)
 
-  changeToState(STATE_MENU)
+function love.update(dt)
+  -- evaluate if head block is completely out of the screen (to the left)
+  -- and if it is, remove it
+  local first = blocks[1]
+  if first[1]+first[3] < 0 then
+    table.remove(blocks,1)
+    num_blocks = num_blocks - 1
+  end
+
+  -- evaluate if tail block is completely on the screen (on the right)
+  -- and if it is, spawn a new one
+  local last = blocks[num_blocks]
+  if last[1]+last[3] < 800 then
+    table.insert(blocks,spawnBlock())
+    num_blocks = num_blocks + 1
+  end
+
+  -- decrease the x pos of the blocks, that is, make them move to the left
+  -- also, decrease block crush_trigger , and if <= 0, CRUSH!!!
+  for _, block in ipairs(blocks) do
+      block[1] = block[1] - 1.5
+
+      local STATUS = block[6]
+      local LENGTH = block[4]
+
+      -- if crush_trigger is over and status is 0
+      if block[5] <= 0 and STATUS == 0 then
+        -- change the status to CRUSHING on the block
+        block[6] = 1
+      elseif STATUS == 1 then -- if the block is crushing
+        -- keep crushing if the block didn't hit the bottom
+        if LENGTH < 800 then
+          block[4] = block[4] + 80
+        else
+          -- otherwise change status to RECEDING, on the block
+          block[6] = 2
+        end
+      elseif STATUS == 2 then -- if the block is receding
+        if LENGTH > 50 then
+          block[4] = block[4] - 50
+        else
+          -- change status to NO CHANGE on the block
+          block[6] = 0
+          -- reset the crush trigger
+          block[5] = love.math.random(0,1000)+500
+        end
+      else -- simply decrease the CRUSH_TRIGGER
+        block[5] = block[5] - 4
+      end
+  end -- for
 end
 
 function love.draw()
-  hud:draw()
-
-  if GAME_STATE == STATE_PLAY then
-    love.graphics.setColor(0, 0, 255)
-    love.graphics.rectangle("fill", pos_x, pos_y, bsize_x, bsize_y)
-
-    love.graphics.setColor(255, 255, 255)
-    love.graphics.rectangle("fill", block_pos_x, block_pos_y, bsize_x, bsize_y)
-
-    if CheckCollision(pos_x, pos_y, bsize_x, bsize_y, block_pos_x, block_pos_y, bsize_x, bsize_y) then
-      block_pos_x = love.math.random(0, window_w - bsize_x)
-      block_pos_y = love.math.random(30, window_h - bsize_y)
-
-      -- If there's a collision with the white box, increase the score
-      hud:increase_score(1)
-    end
+  for _, block in ipairs(blocks) do
+      love.graphics.setColor(255 - block[5]/4, 100, 0)
+      love.graphics.rectangle("fill", block[1], block[2], block[3], block[4])
   end
-end
-
-function love.update()
-  if hud:timer_over() and GAME_STATE ~= STATE_GAME_OVER then
-    changeToState(STATE_GAME_OVER)
-  end
-
-  if GAME_STATE == STATE_MENU then
-    if love.keyboard.isDown( "space" ) then
-      changeToState(STATE_PLAY)
-    end
-  elseif GAME_STATE == STATE_PLAY then
-    if love.keyboard.isDown( "down" ) then
-      goDown()
-    elseif love.keyboard.isDown( "up" ) then
-   		goUp()
-    elseif love.keyboard.isDown( "left" ) then
-   		goLeft()
-    elseif love.keyboard.isDown( "right" ) then
-   		goRight()
-    end
-  elseif GAME_STATE == STATE_GAME_OVER then
-    if love.keyboard.isDown( "space" ) then
-      changeToState(STATE_PLAY)
-    end
-	end
-
-	shape_collider = love.physics.newRectangleShape(pos_x, pos_y, bsize_x, bsize_y)
-
-end
-
-function gameStart()
-  -- initialize hud stuff
-  hud:set_session_time(10)
-  hud:toggle_score(true)
-  hud:toggle_timer(true)
-end
-
-function changeToState(state)
-
-  if state == STATE_MENU then
-    hud:message("Press 'space' to start")
-  elseif state == STATE_PLAY then
-    hud:message("")
-    hud:reset()
-    gameStart()
-  elseif state == STATE_GAME_OVER then
-    print("Set game over message")
-    hud:message("GAME OVER!! Press 'space' to replay")
-  end
-
-  GAME_STATE = state
-end
-
--- Cursor Movement
-
-function goDown()
-  if pos_y + bsize_y < window_h then
-    pos_y = pos_y + 10
-  end
-end
-
-function goUp()
-  if pos_y > HUD_HEIGHT then
-    pos_y = pos_y - 10
-  end
-end
-
-function goLeft()
-  if pos_x > 0 then
-    pos_x = pos_x - 10
-  end
-end
-
-function goRight()
-  if pos_x + bsize_x < window_w then
-    pos_x = pos_x + 10
-  end
-end
-
--- Collision detection
-
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-  return x1 < x2+w2 and
-         x2 < x1+w1 and
-         y1 < y2+h2 and
-         y2 < y1+h1
+  --love.graphics.setColor(0, 0, 255)
+  --love.graphics.rectangle("fill", pos_x, pos_y, bsize_x, bsize_y)
+  -- love.graphics.print("blocks " .. num_blocks, 10, 10)
 end
